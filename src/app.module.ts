739599d9a803b638fe1ed8984948cbeedd3bf70c
GitRockie <150247498+honomonodev@users.noreply.github.com>
2024-12-21 23:37:17 +0100

    jwt/passport set

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './modules/auth/auth.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { FarmModule } from './modules/farm/farm.module';
+import { UserModule } from './modules/user/user.module';
 
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    FarmModule,
+    UserModule
+  ],
   
 })
 export class AppModule {}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class FarmService {}
+export class AuthService {}
