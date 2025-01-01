dd2e95ab3e4a89d67ab8a12bf9ee12597100e6be
GitRockie <150247498+honomonodev@users.noreply.github.com>
2025-01-01 19:49:01 +0100

    FarmsService. It should be defined as a proper TypeScript array, not as a tuple

--- src/farms/farms.service.ts
+++ src/farms/farms.service.ts
@@ -2,27 +2,22 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class FarmsService {
-    private farms: [
+    private farms = [
         {
             id: 1,
             name: 'Hippie Huerto',
-    
         },
         {
             id: 2,
             name: 'Bai San',
-            
-        
         },
         {
             id: 3,
             name: 'La Granja del Paco',
-       
         },
         {
             id: 4,
             name: 'El Campo del Pepito',
-            
         },
     ];
 
@@ -34,5 +29,4 @@ export class FarmsService {
         const farm = this.farms.find(farm => farm.id === id);
         return farm;
     }
-
 }
