0cddef3103a83a949cf2e60e91ce770e164ce18e
GitRockie <150247498+honomonodev@users.noreply.github.com>
2025-01-02 19:09:31 +0100

    Post, Patch Delete update

--- src/farms/farms.controller.ts
+++ src/farms/farms.controller.ts
@@ -1,18 +1,42 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { FarmsService } from './farms.service';
+
 
 @Controller('farms')
 export class FarmsController {
 constructor(private readonly farmsService: FarmsService) {}
     
     @Get()
-    findAll() {
+    findAllFarms() {
         return this.farmsService.findAll();
     }
 
     @Get(':id')
     getFarmById(@Param('id', ParseIntPipe) id: number) {
-        console.log({id});
+        
       return this.farmsService.findOneById(id);
+    }
+
+    @Post()
+    createFarm(@Body() body: any) {
+        return body;
+    }
+
+    @Patch(':id')
+    updateFarm(
+        @Param('id', ParseIntPipe) id: number, 
+        @Body() body: any) 
+        {
+        return body;
+        }
+
+    @Delete(':id')
+    deleteFarm(@Param('id', ParseIntPipe) id: number) {
+       
+        return {
+            method: 'delete',
+            id,
+
+        };
     }
 }
